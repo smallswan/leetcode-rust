4376a6e82bcf10e518cbfe40f38c044abe31d805
zhiluniao <huangshunle_gxu@163.com>
2020-03-14 20:06:22 +0800

    力扣第167题（https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/submissions/）双指针解法

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,33 @@ fn main() {
     //println!("total:{}",total);
 
     let numbers = vec![2, 7, 11, 15];
-    let target = 9;
-    let result = two_sum(numbers,target);
+    let target = 18;
+    let result = two_sum(numbers, target);
 
-    println!("{:?}",result);
+    println!("{:?}", result);
 }
 
 /// 50.神奇数字在哪里(https://developer.aliyun.com/coding/50)
 ///
-fn solution(n : usize) -> usize{
+fn solution(n: usize) -> usize {
     let bits = n.to_string().len();
-    if bits >= 3{
+    if bits >= 3 {
         let numbers = generate_array(bits);
         let mut last_result = BTreeSet::<String>::new();
         let mut temp_result = Vec::<Vec<char>>::new();
         for number in numbers {
             let bytes = number.as_bytes();
-            perm(&mut bytes.iter().map(|x| (*x as char)).collect(),0,number.len(),&mut temp_result);
+            perm(
+                &mut bytes.iter().map(|x| (*x as char)).collect(),
+                0,
+                number.len(),
+                &mut temp_result,
+            );
 
-            for result in &temp_result{
-                let num : String = result.iter().collect();
+            for result in &temp_result {
+                let num: String = result.iter().collect();
                 let num_usize = num.parse::<usize>().unwrap();
-                if  num_usize <= n{
+                if num_usize <= n {
                     last_result.insert(num);
                 }
             }
@@ -79,7 +84,6 @@ fn generate_array(bits: usize) -> Vec<String> {
                     temp_list.push(str1);
                     temp_list.push(str2);
                     temp_list.push(str3);
-
                 }
 
                 temp_set.clear();
@@ -96,11 +100,11 @@ fn generate_array(bits: usize) -> Vec<String> {
     return result_list;
 }
 
-fn perm( array : &mut Vec<char>,k :usize,length:usize,result: &mut Vec<Vec<char>>){
-    if k == length - 1{
-       result.push(array.to_owned());
-    }else{
-        for i in k..length{
+fn perm(array: &mut Vec<char>, k: usize, length: usize, result: &mut Vec<Vec<char>>) {
+    if k == length - 1 {
+        result.push(array.to_owned());
+    } else {
+        for i in k..length {
             if is_swap(array, k, i) {
                 swap(array, k, i);
                 perm(array, k + 1, length, result);
@@ -108,19 +112,18 @@ fn perm( array : &mut Vec<char>,k :usize,length:usize,result: &mut Vec<Vec<char>
             }
         }
     }
-
 }
 
-fn is_swap( array : &Vec<char>, begin : usize,end :usize) -> bool{
-    for i in begin..end{
-        if array[i] == array[end]{
+fn is_swap(array: &Vec<char>, begin: usize, end: usize) -> bool {
+    for i in begin..end {
+        if array[i] == array[end] {
             return false;
         }
     }
     return true;
 }
 
-fn swap( array :  &mut Vec<char>, k : usize,i :usize){
+fn swap(array: &mut Vec<char>, k: usize, i: usize) {
     let temp = array[k];
     array[k] = array[i];
     array[i] = temp;
@@ -130,23 +133,22 @@ use std::collections::HashMap;
 /// 力扣--167. 两数之和 II - 输入有序数组（https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/）
 pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
     let mut result = Vec::<i32>::with_capacity(2);
-    let mut numbers_index = HashMap::new();
 
-    let mut index = 1;
-    for number in &numbers{
-        numbers_index.insert(number,index);
-        index +=1;
-    }
-
-    index = 1;
-    for number in &numbers{
-        let sub = target - number;
-        if let Some(num) = numbers_index.get(&sub){
-            result.push(index);
-            result.push(*num);
+    let mut index1 = 0;
+    let mut index2 = numbers.len() - 1;
+    while index2 >= 1 {
+        let sum = numbers[index1] + numbers[index2];
+        if sum < target {
+            index1 += 1;
+            continue;
+        } else if sum > target {
+            index2 -= 1;
+            continue;
+        } else {
+            result.push((index1 + 1) as i32);
+            result.push((index2 + 1) as i32);
             break;
         }
-        index+=1;
     }
 
     result
