9a23df52f2f33d83f6122f35ce88688350b1a516
zhiluniao <huangshunle_gxu@163.com>
2021-02-18 23:54:11 +0800

    力扣（8. 字符串转整数（atoi））

--- src/medium.rs
+++ src/medium.rs
@@ -105,6 +105,64 @@ pub fn longest_palindrome(s: String) -> String {
         .to_string();
 }
 
+enum State {
+    Init,
+    ExpectNumber,// 已经碰到了+或者-，下一个字符必须是数字
+    Number(i32),
+}
+
+/// 力扣（8. 字符串转整数（atoi）） https://leetcode-cn.com/problems/string-to-integer-atoi/
+pub fn my_atoi(s: String) -> i32 {
+    let mut state = State::Init;
+    let mut neg = 1;
+    for c in s.chars() {
+        match c {
+            ' ' => match state {
+                State::Init => {}
+                State::ExpectNumber => return 0,
+                State::Number(n) => return neg * n,
+            },
+            '+' | '-' => match state {
+                State::Init => {
+                    state = State::ExpectNumber;
+                    if c == '-' {
+                        neg = -1;
+                    }
+                }
+                State::ExpectNumber => return 0,
+                State::Number(n) => return neg * n,
+            },
+            '0'..='9' => {
+                let digit = c.to_digit(10).unwrap() as i32;
+                match state {
+                    State::Init | State::ExpectNumber => state = State::Number(digit),
+                    State::Number(n) => {
+                        match n.checked_mul(10).and_then(|x| x.checked_add(digit)) {
+                            Some(number) => state = State::Number(number),
+                            _ => {
+                                return if neg < 0 {
+                                    std::i32::MIN
+                                } else {
+                                    std::i32::MAX
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            _ => match state {
+                State::Init | State::ExpectNumber => return 0,
+                State::Number(n) => return neg * n,
+            },
+        }
+    }
+
+    match state {
+        State::Number(n) => return neg * n,
+        _ => return 0,
+    }
+}
+
 /// 力扣（210. 课程表II），https://leetcode-cn.com/problems/course-schedule-ii/
 pub fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
     use std::collections::VecDeque;
@@ -192,4 +250,9 @@ fn medium() {
 
     let result = add_two_numbers(Some(Box::new(l1)), Some(Box::new(l2)));
     println!("{:?}", result);
+
+    println!("{}", my_atoi(" -423456".to_string()));
+    println!("{}", my_atoi("4193 with words".to_string()));
+    println!("{}", my_atoi("words and 987".to_string()));
+    println!("{}", my_atoi("-91283472332".to_string()));
 }
