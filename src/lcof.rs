4ad3593c3b0bdee680ca7aa6afd77b397ed6e281
zhiluniao <huangshunle_gxu@163.com>
2021-05-15 22:08:00 +0800

    剑指 Offer 10- I. 斐波那契数列

--- src/lcof.rs
+++ src/lcof.rs
@@ -21,6 +21,42 @@ fn test_lcoff() {
     println!("{}", sum);
 }
 
+/// 剑指 Offer 10- I. 斐波那契数列   https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+pub fn fib(n: i32) -> i32 {
+    if n <= 1 {
+        return n;
+    }
+
+    let mut a = 0i64;
+    let mut b = 1i64;
+    let mut c = 0i64;
+    let mut i = 1;
+    while i < n {
+        // 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+        c = (a + b) % 1000000007;
+        a = b;
+        b = c;
+        i += 1;
+    }
+
+    c as i32
+}
+
+/// 剑指 Offer 10- I. 斐波那契数列
+pub fn fib_v2(n: i32) -> i32 {
+    let mut a = 0;
+    let mut b = 1;
+    let mut i = 0;
+    let mut sum = 0;
+    while i < n {
+        sum = (a + b) % 1000000007;
+        a = b;
+        b = sum;
+        i += 1;
+    }
+    a
+}
+
 /// 剑指 Offer 64. 求1+2+…+n  https://leetcode-cn.com/problems/qiu-12n-lcof/
 /// 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
 /// 使用match语句是否违规呢？
