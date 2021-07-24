3511c7d26a4ae1d9fffd4d797ce6481cd37f6873
zhiluniao <huangshunle_gxu@163.com>
2021-07-24 23:09:01 +0800

    力扣（37. 解数独）

--- src/hard.rs
+++ src/hard.rs
@@ -258,6 +258,110 @@ fn matches_v2(s_chars: &Vec<char>, p_chars: &Vec<char>, i: usize, j: usize) -> b
     s_chars[i - 1] == p_chars[j - 1]
 }
 
+/// 力扣（37. 解数独） https://leetcode-cn.com/problems/sudoku-solver/
+pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
+    let mut line = Vec::<Vec<bool>>::with_capacity(9);
+    let mut column = Vec::<Vec<bool>>::with_capacity(9);
+    let mut block = Vec::<Vec<Vec<bool>>>::with_capacity(3);
+    let mut spaces = Vec::<(usize, usize)>::new();
+    let mut valid = false;
+    for i in 0..9 {
+        let row = vec![false; 9];
+        line.push(row);
+        let col = vec![false; 9];
+        column.push(col);
+    }
+    for i in 0..3 {
+        let row = vec![vec![false; 9]; 3];
+        block.push(row);
+    }
+
+    for i in 0..9 {
+        for j in 0..9 {
+            if board[i][j] == '.' {
+                spaces.push((i, j));
+            } else {
+                let digit = (board[i][j] as usize) - ('0' as usize) - 1;
+                line[i][digit] = true;
+                column[j][digit] = true;
+                block[i / 3][j / 3][digit] = true;
+            }
+        }
+    }
+
+    let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    dfs(
+        board,
+        0,
+        &mut spaces,
+        &mut valid,
+        &mut line,
+        &mut column,
+        &mut block,
+        &digits,
+    );
+}
+
+fn dfs(
+    board: &mut Vec<Vec<char>>,
+    pos: usize,
+    spaces: &Vec<(usize, usize)>,
+    valid: &mut bool,
+    line: &mut Vec<Vec<bool>>,
+    column: &mut Vec<Vec<bool>>,
+    block: &mut Vec<Vec<Vec<bool>>>,
+    digits: &[char],
+) {
+    if pos == spaces.len() {
+        *valid = true;
+        return;
+    }
+
+    let space = spaces.get(pos).unwrap();
+    let i = space.0;
+    let j = space.1;
+    let mut digit = 0;
+    while digit < 9 && !(*valid) {
+        if !line[i][digit] && !column[j][digit] && !block[i / 3][j / 3][digit] {
+            line[i][digit] = true;
+            column[j][digit] = true;
+            block[i / 3][j / 3][digit] = true;
+            //let c = char::from_u32((digit + ('0' as usize) + 1) as u32);
+            board[i][j] = digits[digit + 1];
+            dfs(board, pos + 1, spaces, valid, line, column, block, digits);
+            line[i][digit] = false;
+            column[j][digit] = false;
+            block[i / 3][j / 3][digit] = false;
+        }
+        digit += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn soku() {
+        let mut board = Vec::<Vec<char>>::with_capacity(9);
+        board.push(vec!['5', '3', '.', '.', '7', '.', '.', '.', '.']);
+        board.push(vec!['6', '.', '.', '1', '9', '5', '.', '.', '.']);
+        board.push(vec!['.', '9', '8', '.', '.', '.', '.', '6', '.']);
+        board.push(vec!['8', '.', '.', '.', '6', '.', '.', '.', '3']);
+        board.push(vec!['4', '.', '.', '8', '.', '3', '.', '.', '1']);
+        board.push(vec!['7', '.', '.', '.', '2', '.', '.', '.', '6']);
+        board.push(vec!['.', '6', '.', '.', '.', '.', '2', '8', '.']);
+        board.push(vec!['.', '.', '.', '4', '1', '9', '.', '.', '5']);
+        board.push(vec!['.', '.', '.', '.', '8', '.', '.', '7', '9']);
+
+        solve_sudoku(&mut board);
+
+        for row in 0..9 {
+            println!("{:?}", board[row]);
+        }
+    }
+}
+
 #[test]
 fn hard() {
     let nums1: Vec<i32> = vec![1, 3];
