264d2024a03e584ce42147d8558bed1ad5fe6de4
zhiluniao <huangshunle_gxu@163.com>
2021-08-16 09:08:56 +0800

    cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,7 @@ extern "C" {
 fn main() {
     println!("LeetCode problems that I've solved in Rust");
 
-    let mut tower = Vec::<Vec<i32>>::new();
-    tower.push(vec![3]);
-    tower.push(vec![1, 5]);
-    tower.push(vec![8, 4, 3]);
-    tower.push(vec![2, 6, 7, 9]);
-    tower.push(vec![6, 2, 3, 5, 1]);
-
+    let mut tower = vec![[3], [1, 5], [8, 4, 3], [2, 6, 7, 9], [6, 2, 3, 5, 1]];
     let max = dp::number_tower(tower);
     println!("number_tower max = {:?}", max);
 
