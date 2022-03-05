9c0085cbb3abf5ec4babc35bf832574de67c4f00
zhiluniao <huangshunle_gxu@163.com>
2022-03-05 10:16:30 +0800

    917. 仅仅反转字母；

--- src/solution/string/reverse.rs
+++ src/solution/string/reverse.rs
@@ -28,11 +28,31 @@ pub fn reverse_words(s: String) -> String {
     chars.iter().collect()
 }
 
+/// 917. 仅仅反转字母 https://leetcode-cn.com/problems/reverse-only-letters/
+pub fn reverse_only_letters(s: String) -> String {
+    let mut s = s.into_bytes();
+    let mut iter = s.iter_mut().filter(|&&mut c| c.is_ascii_alphabetic());
+
+    while let Some(left) = iter.next() {
+        if let Some(right) = iter.next_back() {
+            std::mem::swap(left, right);
+        } else {
+            break;
+        }
+    }
+
+    String::from_utf8(s).unwrap()
+}
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_reverse() {
         dbg!(reverse_words(String::from("Let's take LeetCode contest")));
+
+        assert_eq!(
+            reverse_only_letters("Test1ng-Leet=code-Q!".into()),
+            "Qedo1ct-eeLg=ntse-T!".to_string()
+        );
     }
 }
