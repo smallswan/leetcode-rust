efc792f76f49d2bad5ada2ed3fa80dac28d4cdd2
zhiluniao <huangshunle_gxu@163.com>
2022-03-14 00:22:21 +0800

    303. 区域和检索 - 数组不可变;

--- src/solution/data_structures/array/operations.rs
+++ src/solution/data_structures/array/operations.rs
@@ -20,6 +20,31 @@ pub fn rotate(nums: &mut Vec<i32>, k: i32) {
     nums.rotate_right(k as usize % len)
 }
 
+/// 303. 区域和检索 - 数组不可变 https://leetcode-cn.com/problems/range-sum-query-immutable/
+struct NumArray {
+    data: Vec<i32>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl NumArray {
+    fn new(nums: Vec<i32>) -> Self {
+        NumArray { data: nums }
+    }
+
+    fn sum_range(&self, left: i32, right: i32) -> i32 {
+        let (mut left, mut right) = (left as usize, right as usize);
+        let mut result = 0;
+        while left <= right {
+            result += self.data[left];
+            left += 1;
+        }
+        result
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +60,11 @@ mod tests {
         let mut nums = vec![1, 2, 3, 4, 5, 6, 7];
         let k = 3;
         rotate(&mut nums, k);
+    }
+
+    #[test]
+    fn test_array() {
+        let array = NumArray::new(vec![1, 2, 3, 4, 5, 6, 7]);
+        dbg!(array.sum_range(2, 5));
     }
 }
