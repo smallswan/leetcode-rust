a4364085bbd684c4dd6b8a8417b6343ecbd32aa0
zhiluniao <huangshunle_gxu@163.com>
2022-03-19 12:48:39 +0800

    149. 直线上最多的点数;

--- src/solution/math/operations.rs
+++ src/solution/math/operations.rs
@@ -474,6 +474,63 @@ pub fn climb_stairs_v2(n: i32) -> i32 {
     (fibn / sqrt5).round() as i32
 }
 
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::convert::TryInto;
+
+fn gcd(mut x: i32, mut y: i32) -> i32 {
+    while y != 0 {
+        let next_y = x % y;
+
+        x = y;
+        y = next_y;
+    }
+
+    x
+}
+
+fn get_line_key([x_1, y_1]: [i32; 2], [x_2, y_2]: [i32; 2]) -> (i32, i32) {
+    let d_x = x_2 - x_1;
+    let d_y = y_2 - y_1;
+    let g = gcd(d_x, d_y);
+    let r_x = d_x / g;
+    let r_y = d_y / g;
+
+    if r_x < 0 {
+        (-r_x, -r_y)
+    } else {
+        (r_x, r_y)
+    }
+}
+
+/// 149. 直线上最多的点数 https://leetcode-cn.com/problems/max-points-on-a-line/
+pub fn max_points(points: Vec<Vec<i32>>) -> i32 {
+    let mut result = 0;
+    let mut counts = HashMap::new();
+
+    for (i, point_1) in points
+        .iter()
+        .map(|p| p.as_slice().try_into().unwrap())
+        .enumerate()
+    {
+        for point_2 in points[i + 1..]
+            .iter()
+            .map(|p| p.as_slice().try_into().unwrap())
+        {
+            match counts.entry(get_line_key(point_1, point_2)) {
+                Entry::Occupied(entry) => *entry.into_mut() += 1,
+                Entry::Vacant(entry) => {
+                    entry.insert(1);
+                }
+            }
+        }
+
+        result = result.max(1 + counts.drain().map(|(_, v)| v).max().unwrap_or(0));
+    }
+
+    result
+}
+
 /// 力扣（168. Excel表列名称） https://leetcode-cn.com/problems/excel-sheet-column-title/
 pub fn convert_to_title(column_number: i32) -> String {
     let mut ret = String::new();
