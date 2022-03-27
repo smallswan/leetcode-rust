35feef66a0d194ef90ae679c86bfa359d51ca6bb
zhiluniao <huangshunle_gxu@163.com>
2022-03-27 18:04:33 +0800

    银联-02. 优惠活动系统(61/62)

--- src/contest/spring2022.rs
+++ src/contest/spring2022.rs
@@ -40,6 +40,7 @@ pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
     false
 }
 
+/// 判断迭代器中的元素是否是回文
 fn is_palindrome_iter(mut iter: impl DoubleEndedIterator<Item = impl Eq>) -> bool {
     while let (Some(left), Some(right)) = (iter.next(), iter.next_back()) {
         if left != right {
@@ -51,7 +52,10 @@ fn is_palindrome_iter(mut iter: impl DoubleEndedIterator<Item = impl Eq>) -> boo
 }
 
 /// 银联-01. 回文链表
-/// 贪心算法
+/// 贪心算法:
+/// 在允许最多删除一个字符的情况下，同样可以使用双指针，通过贪心实现。初始化两个指针 low和 high分别指向字符串的第一个字符和最后一个字符。每次判断两个指针指向的字符是否相同，如果相同，则更新指针，将 low 加 1，high 减 1，然后判断更新后的指针范围内的子串是否是回文字符串。
+/// 如果两个指针指向的字符不同，则两个字符中必须有一个被删除，此时我们就分成两种情况：即删除左指针对应的字符，留下子串 s[low+1:high]，或者删除右指针对应的字符，留下子串 s[low:high−1]。
+/// 当这两个子串中至少有一个是回文串时，就说明原始字符串删除一个字符之后就以成为回文串。
 pub fn is_palindrome_v2(head: Option<Box<ListNode>>) -> bool {
     let mut vec = Vec::new();
     let mut head = &head;
@@ -77,16 +81,156 @@ pub fn is_palindrome_v2(head: Option<Box<ListNode>>) -> bool {
     true
 }
 
+use std::collections::HashMap;
+struct DiscountSystem {
+    activities: HashMap<i32, Activity>,
+}
+
+enum ActivityState {
+    Normal,
+    Finished,
+}
+struct Activity {
+    state: ActivityState,
+    consume_records: Vec<i32>,
+    act_id: i32,
+    price_limit: i32,
+    discount: i32,
+    number: i32,
+    user_limit: i32,
+}
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl DiscountSystem {
+    fn new() -> Self {
+        DiscountSystem {
+            activities: HashMap::new(),
+        }
+    }
+
+    fn add_activity(
+        &mut self,
+        act_id: i32,
+        price_limit: i32,
+        discount: i32,
+        number: i32,
+        user_limit: i32,
+    ) {
+        let activity = Activity {
+            state: ActivityState::Normal,
+            consume_records: vec![],
+            act_id,
+            price_limit,
+            discount,
+            number,
+            user_limit,
+        };
+        self.activities.insert(act_id, activity);
+    }
+
+    fn remove_activity(&mut self, act_id: i32) {
+        self.activities.remove(&act_id);
+    }
+
+    fn consume(&mut self, user_id: i32, cost: i32) -> i32 {
+        if self.activities.is_empty() {
+            cost
+        } else {
+            // let mut available_activities: Vec<i32> = vec![];
+            let mut max_discount = 0;
+            let mut min_act_id = i32::MAX;
+
+            for (key, value) in self.activities.iter() {
+                if cost >= value.price_limit && value.consume_records.len() < value.number as usize
+                {
+                    let user_limit = value
+                        .consume_records
+                        .iter()
+                        .filter(|&&id| id == user_id)
+                        .count();
+                    if user_limit < value.user_limit as usize {
+                        println!("available_activitie act_id :{}", *key);
+                        // available_activities.push(*key);
+
+                        max_discount = max_discount.max(value.discount);
+                        if max_discount == value.discount {
+                            println!("act_id:{}, discount:{}", value.act_id, value.discount);
+                            min_act_id = value.act_id;
+                            println!("min_act_id  :{}", min_act_id);
+                        }
+                    }
+                }
+            }
+
+            if min_act_id < i32::MAX {
+                if let Some(value) = self.activities.get_mut(&min_act_id) {
+                    value.consume_records.push(user_id);
+
+                    return cost - value.discount;
+                }
+            }
+
+            cost
+        }
+    }
+}
+
+/**
+ * Your DiscountSystem object will be instantiated and called as such:
+ * let obj = DiscountSystem::new();
+ * obj.add_activity(actId, priceLimit, discount, number, userLimit);
+ * obj.remove_activity(actId);
+ * let ret_3: i32 = obj.consume(userId, cost);
+ */
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::solution::data_structures::lists;
     #[test]
     fn unionpay() {
+        dbg!(is_palindrome_iter(vec![1, 2, 3, 4, 4, 3, 2, 1].into_iter()));
+        dbg!(is_palindrome_iter(vec!["s", "o", "s"].into_iter()));
+
         let head = lists::vec_to_list(&vec![1, 2, 3, 1]);
         dbg!(is_palindrome(head));
 
         let head = lists::vec_to_list(&vec![1, 2, 3, 1]);
         dbg!(is_palindrome_v2(head));
+    }
+
+    #[test]
+    fn discount() {
+        let mut system = DiscountSystem::new();
+        // 创建编号 1 的优惠活动，单笔消费原价不小于 10 时，可享受 6 的减免，优惠活动共有 3 个名额，每个用户最多参与该活动 2 次
+        system.add_activity(1, 10, 6, 3, 2);
+
+        // 创建编号 2 的优惠活动，单笔消费原价不小于 15 时，可享受 8 的减免，优惠活动共有 8 个名额，每个用户最多参与该活动 2 次
+        system.add_activity(2, 15, 8, 8, 2);
+
+        dbg!(system.consume(101, 13)); // 用户 101 消费了 13，仅满足优惠活动 1 条件，返回实际支付 13 - 6 = 7用户 101 参加 1 次活动 1，活动 1 剩余 2 个名额
+        dbg!(system.consume(101, 8)); // 用户 101 消费了 8，不满足任何活动，返回支付原价 8
+        system.remove_activity(2); // 结束编号为 2 的活动
+        dbg!(system.consume(101, 17)); // 用户 101 消费了 17，满足优惠活动 1 条件，返回实际支付 17 - 6 = 11用户 101 参加 2 次活动 1，活动 1 剩余 1 个名额
+        dbg!(system.consume(101, 11)); // 用户 101 消费了 11，满足优惠活动 1 条件，但已达到参加次数限制，返回支付原价 11
+        dbg!(system.consume(102, 16)); // 用户 102 消费了 16，满足优惠活动 1 条件，返回实际支付 16 - 6 = 10用户 102 参加 1 次活动 1，活动 1 无剩余名额
+        dbg!(system.consume(102, 21)); // 用户 102 消费了 21，满足优惠活动 1 条件，但活动 1 已无剩余名额，返回支付原价 21
+    }
+
+    // ["DiscountSystem","consume","consume","addActivity","consume","consume","addActivity","consume","consume"]
+    //[[],[4,69],[4,82],[5,45,19,3,2],[6,63],[6,95],[3,38,15,3,3],[6,70],[8,49]]
+    #[test]
+    fn test_discount2() {
+        let mut system = DiscountSystem::new();
+        dbg!(system.consume(4, 69));
+        dbg!(system.consume(4, 82));
+        system.add_activity(5, 45, 19, 3, 2);
+        dbg!(system.consume(6, 63));
+        dbg!(system.consume(6, 95));
+        system.add_activity(3, 38, 15, 3, 3);
+        dbg!(system.consume(6, 70));
+        dbg!(system.consume(8, 49));
     }
 }
