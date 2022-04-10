543a47931245f6f148a0baa7c973df3a2c5130dd
zhiluniao <huangshunle_gxu@163.com>
2022-04-10 11:28:19 +0800

    123. 买卖股票的最佳时机 III

--- src/solution/algorithms/dp/dynamic_programming.rs
+++ src/solution/algorithms/dp/dynamic_programming.rs
@@ -194,6 +194,19 @@ pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
     cache[0]
 }
 
+/// 123. 买卖股票的最佳时机 III https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/
+pub fn max_profit(prices: Vec<i32>) -> i32 {
+    let len = prices.len();
+    let (mut buy1, mut buy2, mut sell1, mut sell2) = (-prices[0], -prices[0], 0, 0);
+    for i in 0..len {
+        buy1 = buy1.max(-prices[i]);
+        sell1 = sell1.max(buy1 + prices[i]);
+        buy2 = buy2.max(sell1 - prices[i]);
+        sell2 = sell2.max(buy2 + prices[i]);
+    }
+    sell2
+}
+
 use std::cmp::min;
 /// 力扣（264. 丑数 II） https://leetcode-cn.com/problems/ugly-number-ii/
 /// 方法二：动态规划
