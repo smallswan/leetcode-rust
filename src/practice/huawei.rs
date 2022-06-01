aee40655f34394db41e46fa298a98a02de5bfccc
zhiluniao <huangshunle_gxu@163.com>
2022-06-02 00:44:29 +0800

    HJ2 计算某字符出现次数;HJ4 字符串分隔;

--- src/practice/huawei.rs
+++ src/practice/huawei.rs
@@ -51,12 +51,61 @@ pub fn length_of_last_word() {
     println!("{}", count);
 }
 
+/// HJ2 计算某字符出现次数  https://www.nowcoder.com/practice/a35ce98431874e3a820dbe4b2d0508b1
+pub fn count_chars_insensitive() {
+    let mut line1 = String::new();
+    io::stdin().read_line(&mut line1).expect("expect a line");
+
+    let mut line2 = String::new();
+    io::stdin().read_line(&mut line2).expect("expect a line");
+    let ch = line2.chars().next().unwrap();
+
+    let mut count = 0;
+    line1.chars().for_each(|c| {
+        if ch.eq_ignore_ascii_case(&c) {
+            count += 1;
+        }
+    });
+
+    println!("{}", count);
+}
+
+/// HJ4 字符串分隔  https://www.nowcoder.com/practice/d9162298cb5a437aad722fccccaae8a7
+use std::iter::FromIterator;
+pub fn hj4_str_split() {
+    let mut line1 = String::new();
+    io::stdin().read_line(&mut line1).expect("expect a line");
+
+    let line1 = line1.trim_end();
+    let len = line1.len();
+
+    if len % 8 == 0 {
+        let chars: Vec<char> = line1.chars().collect::<Vec<_>>();
+        let mut iter = chars.chunks(8);
+        for chunk in iter {
+            println!("{}", String::from_iter(chunk));
+        }
+    } else {
+        let mut line2 = String::from(line1);
+        for i in 0..(8 - (len % 8)) {
+            line2.push('0');
+        }
+        let chars: Vec<char> = line2.chars().collect::<Vec<_>>();
+        let mut iter = chars.chunks(8);
+        for chunk in iter {
+            println!("{}", String::from_iter(chunk));
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_huawei() {
-        length_of_last_word();
+        // length_of_last_word();
+        // count_chars_insensitive();
+        hj4_str_split();
     }
 }
